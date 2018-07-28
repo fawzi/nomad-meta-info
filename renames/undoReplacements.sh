7fb26fa234f426f3d9568f3ac9a64cc18e5044c3
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2018-07-29 01:31:36 +0200

    update undoReplacements (generating v1 from compact with replacements)

--- renames/undoReplacements.sh
+++ renames/undoReplacements.sh
@@ -16,4 +16,18 @@ echo -n "rename compact ... "
 python ../../renames/renamer.py $reverse *.meta_dictionary.json
 cd ../nomad_meta_info
 echo -n "rename v1      ... "
-python ../../renames/renamer.py $reverse *.nomadmetainfo.json
+#python ../../renames/renamer.py $reverse *.nomadmetainfo.json
+cd ../../..
+sbt metaInfoTool/assembly
+jar=meta-info-tool/target/scala-2.11/metaInfoTool-assembly-$(git describe --tags --always --dirty).jar
+java -jar $jar reformat --v1 --base-dir=nomad-meta-info/meta_info/nomad_meta_info nomad-meta-info/meta_info/meta_dictionary/*.meta_dictionary.json
+cd nomad-meta-info
+find . -name \*.json | tar cf revertedMeta.tar -T -
+git stash
+git stash drop
+git checkout v2_norename
+tar xf revertedMeta.tar
+git add -u .
+rm revertedMeta.tar
+git status
+echo "moved to branch v2_norename, and ready to commit"
